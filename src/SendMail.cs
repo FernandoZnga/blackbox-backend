ed42162c218979fbd417b3d43d5de73313351dd3
Fernando Zuniga <fernando.zunigag@gmail.com>
2019-03-15 23:52:18 -0600

    updated the SendMa clss

--- src/SendMail.cs
+++ src/SendMail.cs
@@ -8,6 +8,26 @@ namespace Blackbox.Server.src
 {
     public class SendMail
     {
-        
+        try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("atm.proyecto.seguridad@gmail.com");
+                mail.To.Add("to_address");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("atm.proyecto.seguridad", "Pr0y3ct01");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("Mail-Sended");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
     }
 }
